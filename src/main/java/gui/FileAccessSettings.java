d34b8a10dde3419c054b9c194cc5bd000596fb70
sturlasolheim <247040@usn.no>
2023-05-30 15:10:10 +0200

    Update FileAccessSettings.java

--- src/main/java/gui/FileAccessSettings.java
+++ src/main/java/gui/FileAccessSettings.java
@@ -3,7 +3,9 @@ package gui;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,9 +13,11 @@ public class FileAccessSettings extends JPanel implements ActionListener {
 
     private JButton openButton;
     private JFileChooser chooser;
+    private JLabel chosenFolder;
 
     public FileAccessSettings() {
         super();
+        setLayout(new GridBagLayout());
 
         // Initialize the file chooser
         chooser = new JFileChooser();
@@ -25,7 +29,25 @@ public class FileAccessSettings extends JPanel implements ActionListener {
         // Initialize and setup the button
         openButton = new JButton("Change folder");
         openButton.addActionListener(this); // Set this class as the action listener for the button
-        this.add(openButton); // Add the button to this JPanel
+
+        // Initialize the label
+        chosenFolder = new JLabel("No file chosen");
+
+        // GridBagLayout constraints for the button
+        GridBagConstraints buttonConstraints = new GridBagConstraints();
+        buttonConstraints.gridx = 0;
+        buttonConstraints.gridy = 0;
+        buttonConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        // GridBagLayout constraints for the label
+        GridBagConstraints labelConstraints = new GridBagConstraints();
+        labelConstraints.gridx = 0;
+        labelConstraints.gridy = 1;
+        labelConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        // Add the components to this JPanel
+        this.add(openButton, buttonConstraints);
+        this.add(chosenFolder, labelConstraints);
     }
 
     // Define the action to perform when the button is clicked
@@ -33,8 +55,7 @@ public class FileAccessSettings extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == openButton) {
             if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
-                System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
+                chosenFolder.setText("Chosen file: " + chooser.getSelectedFile().toString());
             } else {
                 System.out.println("No Selection ");
             }
