acf7a236e4ad3bc5fa67751203293375df96f488
sturlasolheim <247040@usn.no>
2023-05-30 11:57:07 +0200

    sdf

--- src/main/java/com/sturla/Eksamen/App.java
+++ src/main/java/com/sturla/Eksamen/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello test!wrofuhwefouhwefu" );
+        System.out.println( "Hello test!wrofuhwefoudfsdsfsdfsdfhwefu" );
     }
 }
