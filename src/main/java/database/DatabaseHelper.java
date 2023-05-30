c45ad2efa8e54ae5f866a2e039bdc099742e83f7
marktho98 <markus.thoresen@hotmail.com>
2023-05-30 12:51:25 +0200

    database

--- src/main/java/database/DatabaseHelper.java
+++ src/main/java/database/DatabaseHelper.java
@@ -1,5 +1,48 @@
 package database;
 
-public class DatabaseHelper {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DatabaseHelper {
+	
+	public static void main(String[] args) throws SQLException {
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		
+		
+		try {
+			
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "student" , "student");
+			
+			System.out.println("Database connection successful!\n");
+			
+			myStmt = myConn.createStatement();
+			
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			while  (myRs.next()) {
+				System.out.println(myRs.getString("lastName") + ", " + myRs.getString("firstName"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myRs != null) {
+				myRs.close();
+		    }
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			if (myConn != null) {
+				myConn.close();
+			
+			}
+		}
+	}
 }
