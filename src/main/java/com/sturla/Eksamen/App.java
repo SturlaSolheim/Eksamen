e2971a8add23953830b88f2152de1d548378bed9
sturlasolheim <247040@usn.no>
2023-05-30 11:04:42 +0200

    Initial commit

---
+++ src/main/java/com/sturla/Eksamen/App.java
@@ -0,0 +1,13 @@
+package com.sturla.Eksamen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
