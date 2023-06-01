150dfbcb76630246702a6b2dd2a2ca8f0705cdbe
AleksanderVold <aleksandervold113@gmail.com>
2023-06-01 11:01:35 +0200

    Error messages

--- src/main/java/frames/ExecuteSql.java
+++ src/main/java/frames/ExecuteSql.java
@@ -1,14 +1,3 @@
-/**
- * [Brief description of the class]
- * 
- * [Detailed description of the class]
- * 
- * [Author(s) and their contribution]
- * 
- * [Purpose of the class]
- * 
- * [Explanation of the included methods]
- */
 
 package frames;
 
@@ -27,22 +16,25 @@ public class ExecuteSql extends JFrame {
     private JTextArea resultTextArea;
 
     public ExecuteSql() {
-        super();
+    	super();
         setTitle("Execute SQL");
-        setSize(600, 400);
+        setSize(1000, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.setLayout(new GridBagLayout());
 
-        JLabel sqlLabel = new JLabel("Enter SQL:");
-        panel.add(sqlLabel);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
 
-        sqlTextArea = new JTextArea(8, 30);
+        sqlTextArea = new JTextArea(5, 30);
         JScrollPane sqlScrollPane = new JScrollPane(sqlTextArea);
-        panel.add(sqlScrollPane);
+        c.gridx = 0;
+        c.gridy = 0;
+        panel.add(sqlScrollPane, c);
 
         executeButton = new JButton("Execute");
         executeButton.addActionListener(new ActionListener() {
@@ -50,17 +42,14 @@ public class ExecuteSql extends JFrame {
                 executeSqlQuery();
             }
         });
-        panel.add(Box.createVerticalStrut(10));
-        panel.add(executeButton);
-
-        JLabel resultLabel = new JLabel("Result:");
-        panel.add(Box.createVerticalStrut(10));
-        panel.add(resultLabel);
+        c.gridy = 1;
+        panel.add(executeButton, c);
 
         resultTextArea = new JTextArea(10, 30);
         resultTextArea.setEditable(false);
         JScrollPane resultScrollPane = new JScrollPane(resultTextArea);
-        panel.add(resultScrollPane);
+        c.gridy = 2;
+        panel.add(resultScrollPane, c);
 
         add(panel);
     }
@@ -90,4 +79,9 @@ public class ExecuteSql extends JFrame {
         }
         resultTextArea.setText(sb.toString());
     }
+
+	public void showMessage() {
+	
+		
+	}
 }
